309a765a7b2bb754e2b227f617164aee7b35dd2a
beaxel <axel.bering@ynov.com>
2025-09-25 15:46:37 +0200

    feat(main):
    adding test2

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ func test1() {
 	fmt.Println("This is a first test")
 }
 
+func test2() {
+	fmt.Println("This is a second test")
+}
+
 func main() {
 	fmt.Println("Hello world from a!")
 	test1()
+	test2()
 }
